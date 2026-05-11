f7dc1f88512fb7c98f489458b895176cf048693a
laxmanchaudhary374-cell <laxmanchaudhary374@gmail.com>
2026-05-11 10:21:14 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,5 @@
-
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { initAdMob } from "./utils/admob";
-import { initBilling } from "./utils/billing";
-
-// Initialize native plugins as soon as the app starts
-const initPlugins = async () => {
-  try {
-    // Start AdMob and Billing connections immediately
-    await initAdMob();
-    await initBilling();
-    console.log('[Main] Native plugins initialized');
-  } catch (e) {
-    console.error('[Main] Plugin init error:', e);
-  }
-};
-
-initPlugins();
 
 createRoot(document.getElementById("root")!).render(<App />);
